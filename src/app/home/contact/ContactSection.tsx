7feeaf1c881653282f1e6954c3b1cfc27761fa7d
PrapooRozario <albart2022@gmail.com>
2025-05-29 23:34:04 +0600

    [ADDED]: Contact Section

---
+++ src/app/home/contact/ContactSection.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+
+export const ContactSection = () => {
+  return (
+    <div className="rounded-3xl w-full min-h-[500px] p-6 relative bg-[url('/banner/contact-bg.png')] bg-no-repeat bg-cover">
+      <div className="text-white flex flex-col justify-center items-center absolute inset-0 text-center px-4">
+        <h2 className="sm:text-3xl text-xl md:text-4xl font-bold mb-6">
+          প্রদায়ক/কন্ট্রিবিউটর চাই
+        </h2>
+
+        <p className="text-lg md:text-xl mb-8 leading-relaxed max-w-2xl">
+          আপনি কী শিক্ষার্থী কিংবা গৃহিনী/চাকুরিজীবী, লিখতে চান নিয়মিত আপনার
+          মনের কথা, সাহিত্য কিংবা সংবাদ নিয়ে কাজ করতে চান? তাহলে আজই আপনার
+          পূর্ণাঙ্গ বায়োডাটা পাঠিয়ে দিন। এছাড়া আপনার ৩টি লেখা নমুনা হিসেবে
+          পাঠাতে হবে।
+        </p>
+        <Link href="/">
+          <button
+            aria-label="যোগাযোগ করুন"
+            className="bg-white hover:bg-[#FF5CBC] hover:text-white text-[#FF5CBC] cursor-pointer font-medium py-3 px-8 rounded-lg text-lg transition-colors duration-300"
+          >
+            যোগাযোগ করুন
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,24 @@ import Banner from "./home/banner/banner";
 import NewPublications from "./home/new-publications/NewPublications";
 import EidSpecial from "./home/eid-special/EidSpecial";
 import LatestNews from "./home/latest-news/LatestNews";
+import { ContactSection } from "./home/contact/ContactSection";
 export default function page() {
   return (
     <div>
       <section className="mt-10 mb-[107px]">
         <Banner />
       </section>
-      <section className="mt-10 mb-[107px]">
+      <section className="mb-[107px]">
         <NewPublications />
       </section>
-      <section className="mt-10 mb-[107px]">
+      <section className="mb-[107px]">
         <EidSpecial />
       </section>
-      <section className="mt-10 mb-[107px]">
+      <section className="mb-[107px]">
         <LatestNews />
+      </section>
+      <section className="mb-[107px]">
+          <ContactSection />
       </section>
     </div>
   );
